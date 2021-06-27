98cf363dfc9469e85f5ce3c10fe61f717ea01838
tono0114 <tono.cookie_crunch@icloud.com>
2021-06-27 13:48:38 +0000

    [Finish]4-10 ~bookers2~

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '7807859d8eae346caf572ebe82abc49cf138678a5172ef1f957cfc841c39883d365eb9082ff7aa47fa8b76db9c43775bddccc45427bfa363ca790247483921e4'
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:index,:show,:edit,:update]
+  resources :books, only: [:create,:index,:show,:edit,:update,:destroy]
   root to:'homes#top'
-  get 'homes/show'
-  resources :books,only: [:create,:index,:show,:edit,:update,:destroy]
-  resources :users,only: [:index,:show,:edit,:update,:destroy]
+  get 'home/about' => 'homes#show'
 end
